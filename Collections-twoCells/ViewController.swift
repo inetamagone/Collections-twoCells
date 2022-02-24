55c319a7c5372f0c19ff89cde3c7541c392d28ac
inetamagone <99735096+inetamagone@users.noreply.github.com>
2022-02-24 08:49:50 +0200

    Initial Commit

---
+++ Collections-twoCells/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collections-twoCells
+//
+//  Created by ineta.magone on 24/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
